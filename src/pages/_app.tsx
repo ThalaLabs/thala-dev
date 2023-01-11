995438e474f6e76f0f786821e681175e59412acc
0xbe1 <0xbetrue@gmail.com>
2023-01-11 17:33:43 +0800

    feat: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import theme from "../theme";
 import { AppProps } from "next/app";
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
