23a93e14ffb87b7505432cbc7de521a51e7b0121
Smart Geek <91398406+mastercodercat@users.noreply.github.com>
2023-01-31 21:18:40 -0500

    fixed: update logo for black/white color modes (#17)
    * fix: update logo for black/white color modes
    
    * refactor: hero component with logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,24 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, useColorMode } from "@chakra-ui/react";
+import Image from "next/image";
 
-export const Hero = ({ title }: { title: string }) => (
-  <Flex
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
-    bgClip="text"
-  >
-    <Heading fontSize="6vw">{title}</Heading>
-  </Flex>
-);
+import blackLogo from "../assets/logos/fullBlackLogo.svg";
+import whiteLogo from "../assets/logos/fullWhiteLogo.svg";
+
+export const Hero = ({ title }: { title: string }) => {
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
+  return (
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
+      bgClip="text"
+    >
+      <Heading>
+        <Image src={isDark ? whiteLogo : blackLogo} width={320} height={50} alt="thala-logo" />
+      </Heading>
+    </Flex>
+  );
+};
