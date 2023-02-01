a337848d798bdd263ffb472a25374fa636827f9f
mastercodercat <shanelewix@gmail.com>
2023-02-01 08:28:49 -0500

    fix: updated home page styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,6 @@ export const Hero = ({ title }: { title: string }) => {
     <Flex
       justifyContent="center"
       alignItems="center"
-      height="100vh"
       bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
       bgClip="text"
     >
