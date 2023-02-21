9366c7df2e6dbbd4dbe25877e8b4de65fb0e8279
mastercodercat <shanelewix@gmail.com>
2023-02-21 14:41:07 -0500

    added: thala-faucet to thala-dev repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,11 @@
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { PontemWallet } from "@pontem/wallet-adapter-plugin";
+import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
+import { RiseWallet } from "@rise-wallet/wallet-adapter";
+import { TrustWallet } from "@trustwallet/aptos-wallet-adapter";
+import { SpikaWallet } from "@spika/aptos-plugin";
 import { Analytics } from "@vercel/analytics/react";
 
 import theme from "../theme";
@@ -7,7 +14,19 @@ import { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <AptosWalletAdapterProvider
+        plugins={[
+          new PetraWallet(),
+          new PontemWallet(),
+          new MartianWallet(),
+          new RiseWallet(),
+          new TrustWallet(),
+          new SpikaWallet(),
+        ]}
+        autoConnect={true}
+      >
+        <Component {...pageProps} />
+      </AptosWalletAdapterProvider>
       <Analytics />
     </ChakraProvider>
   );
