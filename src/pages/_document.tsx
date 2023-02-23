a3fbcb5ef8314f28e7fafb5cc7c0bf3329e1f2e2
mastercodercat <shanelewix@gmail.com>
2023-02-23 10:21:45 -0500

    updated: social preview to thala-dev

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,14 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta property="og:title" content="Thala OSS" />
+          <meta
+            property="og:description"
+            content="One of our core principles is to build, publish, and maintain open-source software."
+          />
+          <meta property="og:image" content="https://www.thala.dev/thala.png" />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
